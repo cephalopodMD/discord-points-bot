5ac914586c7706b12ecf23f617f48b8aeedc7773
Taylor Gregory <tgdev91@gmail.com>
2020-05-19 23:30:23 -0400

    Cleanup MessageTemplates

--- Core/MessageTemplates.cs
+++ Core/MessageTemplates.cs
@@ -2,9 +2,7 @@
 {
     internal static class MessageTemplates
     {
-        public static string AddPoints(string player, int amount) => $"{{ \"Action\": \"add\", \"Payload\": {{ \"PlayerId\": \"{player}\", \"Amount\": {amount} }} }}";
-        public static string RemovePoints(string player, int amount) => $"{{ \"Action\": \"remove\", \"Payload\": {{ \"PlayerId\": \"{player}\", \"Amount\": {amount} }} }}";
-        public static string AddPlayer(string player) => $"{{ \"Action\": \"init\", \"Payload\": {{ \"PlayerId\": \"{player}\" }} }}";
-        public static string NukeIt() => $"{{ \"Action\": \"reset\" }}";
+        public static string AddPoints(string originPlayer, string targetPlayer, int amount) => $"{{ \"Action\": \"add\", \"Payload\": {{ \"OriginPlayerId\": \"{originPlayer}\", \"TargetPlayerId\": \"{targetPlayer}\", \"Amount\": {amount} }} }}";
+        public static string RemovePoints(string originPlayer, string targetPlayer, int amount) => $"{{ \"Action\": \"remove\", \"Payload\": {{ \"OriginPlayerId\": \"{originPlayer}\", \"TargetPlayerId\": \"{targetPlayer}\", \"Amount\": {amount} }} }}";
     }
 }
