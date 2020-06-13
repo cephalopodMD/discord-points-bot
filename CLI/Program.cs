fce5bf305fb384543cc36e535d7b26c165b5e305
Taylor Gregory <tgdev91@gmail.com>
2020-06-13 18:09:03 -0400

    Command to dump redis

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,8 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Configuration;
 using PointsBot.Infrastructure.Commands;
+using StackExchange.Redis;
 
 namespace PointsBot.CLI
 {
@@ -13,18 +20,61 @@ namespace PointsBot.CLI
             .AddJsonFile("./local.appsettings.json", true, true)
             .Build();
 
-        private static Task Main(string[] args)
+        private static async Task Main(string[] args)
         {
             var sender = new CommandSender(new QueueClient(new ServiceBusConnectionStringBuilder(Configuration["CommandServiceBusConnectionString"])));
 
             switch (args[0])
             {
                 case "add":
-                    return sender.SendAdd(args[1], args[2], Int32.Parse(args[3]), "CLI");
+                    await sender.SendAdd(args[1], args[2], Int32.Parse(args[3]), "CLI");
+                    break;
                 case "remove":
-                    return sender.SendRemove(args[1], args[2], Int32.Parse(args[3]), "CLI");
-                default: return Task.CompletedTask;
+                    await sender.SendRemove(args[1], args[2], Int32.Parse(args[3]), "CLI");
+                    break;
+                case "dumpredis":
+                    await DumpRedis();
+                    break;
+                default: return;
             }
         }
+
+        private static async Task DumpRedis()
+        {
+            var multiplexer = ConnectionMultiplexer.Connect(Configuration["RedisConnection"]);
+            var database = multiplexer.GetDatabase();
+
+            var amountOfPointsCommands = database.ListLength("points");
+            var pointsTasks = new List<Task<RedisValue>>();
+
+            for (int ii = 0; ii < amountOfPointsCommands; ii++)
+            {
+                pointsTasks.Add(database.ListGetByIndexAsync("points", ii));
+            }
+
+            var pointsCommandsAsJson = await Task.WhenAll(pointsTasks);
+            var pointsCommands = pointsCommandsAsJson.Select(p => JsonSerializer.Deserialize<PointsCommand>(p));
+
+            var dumpFile = File.Create($"C:\\points_bot_dump_{DateTime.Now.ToFileTime()}.json");
+            var data = Encoding.UTF8.GetBytes(JsonSerializer.Serialize(pointsCommands));
+
+            await dumpFile.WriteAsync(data);
+        }
+    }
+
+    public class PointsCommand
+    {
+        public string OriginPlayerId { get; set; }
+
+        public string TargetPlayerId { get; set; }
+
+        public EventParameters EventParameters { get; set; }
+    }
+
+    public class EventParameters
+    {
+        public string Action { get; set; }
+
+        public int Amount { get; set; }
     }
 }
